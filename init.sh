7ae50a7fe17a474fb2aca6c1e8e144e2d66ef226
re3turn <re3turn@gmail.com>
2020-08-25 08:19:29 +0000

    Fix not defined environment

--- init.sh
+++ init.sh
@@ -6,10 +6,15 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd ${SCRIPT_DIR}
 
 ruby ./bin/init.rb --token=${SLACK_ACCESS_TOKEN}
-sed -i "s#mongo:27017#${MONGODB_URI}#" ./config.yml
-sed -i "s#slack_logger#${MONGODB_NAME}#" ./config.yml
 
 set +u
+
+if [ ! -z "${MONGODB_URI}" ]; then
+    sed -i "s#mongo:27017#${MONGODB_URI}#" ./config.yml
+fi
+if [ ! -z "${MONGODB_NAME}" ]; then
+    sed -i "s#slack_logger#${MONGODB_NAME}#" ./config.yml
+fi
 
 if [ ! -z "${DEFAULT_CHANNEL}" ]; then
     sed -i "s#default_channel: general#default_channel: ${DEFAULT_CHANNEL}#" ./config.yml
